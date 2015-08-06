63b1d6a25d55a04261d8dad5801bc4927376f508
Kent Ma <m.kentma@gmail.com>
2015-08-06 10:36:15 -0400

    minor work

---
+++ app/src/main/java/example/mk/appcompatexample/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,7 @@
+package example.mk.appcompatexample.adapter;
+
+/**
+ * Created by kentm_000 on 8/6/2015.
+ */
+public class NavigationDrawerAdapter {
+}
--- app/src/main/java/example/mk/appcompatexample/model/NavigationDrawerItem.java
+++ app/src/main/java/example/mk/appcompatexample/model/NavigationDrawerItem.java
@@ -5,9 +5,7 @@ package example.mk.appcompatexample.model;
  */
 public class NavigationDrawerItem {
 
-    private boolean mShowNotify;
-    private String mTitle;
-
+    private boolean showNotify;
     private String title;
 
     public NavigationDrawerItem(){
@@ -15,9 +13,23 @@ public class NavigationDrawerItem {
     }
 
     public NavigationDrawerItem (boolean showNotify, String title){
-        mShowNotify = showNotify;
-        mTitle = title;
-
+        this.showNotify = showNotify;
+        this.title = title;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
 }
